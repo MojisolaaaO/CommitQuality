e7e6f23e0d83868fbf6fe73d3caa14bdfdf9fe37
MojisolaaaO <mojisolaotusheso@gmail.com>
2024-09-15 22:29:03 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -3,14 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
-            // implement node event listeners here
+            require("cypress-mochawesome-reporter/plugin")(on);
         },
         baseUrl: "https://commitquality.com/",
         viewportHeight: 550,
         viewportWidth: 660,
         experimentalStudio: false, //record commands
         watchForFileChanges: false, //auto-run
-        reporter: "mochawesome",
+        reporter: "cypress-mochawesome-reporter",
         "reporterOptions": {
             "charts": true,
             "overwrite": true,
